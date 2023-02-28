8e6b3dd17e14a36594fe104de91487dcd1c0e57e
pragota.ardhana <pragota.ardhana@gmail.com>
2023-02-28 14:51:15 +0700

    First commit

---
+++ .history/index_20230228131020.js
@@ -0,0 +1,8 @@
+var drumButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<drumButton; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("i got clicked");
+    })
+}
+
